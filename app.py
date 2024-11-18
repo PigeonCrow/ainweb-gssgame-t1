5b8f71dfdc962c96d61f4e518279a3403f8eb913
TITO DUARTE <ic-consult.duarte@extaccount.com>
2024-11-18 21:37:29 +0100

    add local animation file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import json
 from streamlit_lottie import st_lottie
 
 
@@ -10,14 +11,18 @@ def load_lottieurl(url):
     return r.json()
 
 
+def load_lottiefile(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+
 # Load Lottie animation
 lottie_url = "https://lottie.host/9ff7a123-ddc0-4686-91af-4918728c225a/1aZEVCy5Fn.json"
 lottie_animation = load_lottieurl(lottie_url)
 
-st.set_page_config(page_title="Capital Guessing Game")
+st.set_page_config(page_title="Capital Guessing Game", layout="wide", menu_items=None)
 
 st.title("Capital Guessing Game")
-st.markdown("## Welcome to the Capital Guessing Game!")
 
 # Display the Lottie animation
 st_lottie(lottie_animation, height=300, key="welcome")
